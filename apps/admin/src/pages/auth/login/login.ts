b87d1037c03e47f720999d217eb907bfd540bd2b
berkaypehllivan <berkaypehlivan75@gmail.com>
2026-04-11 17:31:17 +0300

    <feature>(auth): Şifre sıfırlama işlemlerinin sayfası tasarlandı ve backend ile çalıştırılacak duruma getirildi.

--- apps/admin/src/pages/auth/login/login.ts
+++ apps/admin/src/pages/auth/login/login.ts
@@ -1,16 +1,17 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   inject,
   signal,
+  viewChild,
   ViewEncapsulation,
 } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Result } from '../../models/result.model';
 import { Router } from '@angular/router';
 import { FormValidateDirective } from 'form-validate-angular';
-import { HttpService } from '../../services/http';
+import { HttpService } from '../../../services/http';
+import { FlexiToastService } from 'flexi-toast';
 
 @Component({
   imports: [FormsModule, FormValidateDirective],
@@ -22,15 +23,31 @@ export default class Login {
   readonly loading = signal<boolean>(false);
   readonly #http = inject(HttpService);
   readonly #router = inject(Router);
+  readonly #toast = inject(FlexiToastService);
+
+  readonly email = signal<string>("");
+  readonly  closeBtn = viewChild<ElementRef<HTMLButtonElement>>("modalCloneBtn");
 
   login(form: NgForm) {
     if (!form.valid) return;
 
     this.loading.set(true);
-    this.#http.post<string>('/rent/auth/login', form.value, (res) => {
-      localStorage.setItem('response', res);
-      this.#router.navigateByUrl('/');
-      this.loading.set(false);
-    },()=> this.loading.set(false));
+    this.#http.post<string>(
+      '/rent/auth/login',
+      form.value,
+      (res) => {
+        localStorage.setItem('response', res);
+        this.#router.navigateByUrl('/');
+        this.loading.set(false);
+      },
+      () => this.loading.set(false),
+    );
+  }
+
+  forgotPassword(){
+    this.#http.post<string>(`/rent/auth/forgot-password/${this.email()}`,{}, (res) => {
+      this.#toast.showToast("Başarılı",res, "info");
+      this.closeBtn()!.nativeElement.click();
+    });
   }
 }
